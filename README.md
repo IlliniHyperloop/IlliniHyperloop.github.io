ca64eb43b0f3614f4a96803fa4771d57612b26c2
Salil999 <ssaxen4@illinois.edu>
2015-09-28 21:34:39 -0500

    complete re-design and updated hyper loop website

--- README.md
+++
@@ -1,2 +0,0 @@
-# IlliniHyperloop
-Illini Hyperloop Website
--- foxyform.js
+++
@@ -1,6 +0,0 @@
-(function(d, t){
-var g = d.createElement(t),
-s = d.getElementsByTagName(t)[0];
-g.src = "http://www.foxyform.com/js.php?id=695160&sec_hash=457535a49c2&width=350px";
-s.parentNode.insertBefore(g, s);
-}(document, "script"));
